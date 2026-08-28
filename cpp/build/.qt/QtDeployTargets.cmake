49acdb153a6f06b9b4ee28859b874133dbfdfa71
RapidWoLf23 <ayushsutradhar23@gmail.com>
2026-08-28 12:26:21 +0530

    Remove Python implementation and keep C++ Qt version

--- cpp/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_WindowsSpotlight_FILE C:/Users/ayush/OneDrive/Documents/project/WindowsSpotlight/cpp/build/WindowsSpotlight.exe)
-set(__QT_DEPLOY_TARGET_WindowsSpotlight_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_WindowsSpotlight_RUNTIME_DLLS C:/Qt/6.11.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.2/mingw_64/bin/Qt6Core.dll)
